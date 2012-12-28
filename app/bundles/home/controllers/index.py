cc0a8f9d0e9fa959983e22ed3f73d4a853a5dfc2
Andrea Belvedere <andrea.belvedere@gengo.com>
2012-12-28 20:31:12 +0900

    add argument to index controller

--- app/bundles/home/controllers/index.py
+++ app/bundles/home/controllers/index.py
@@ -6,5 +6,5 @@ from lib.controller import Controller
 
 class Index(Controller):
 
-    def index(self):
-        return __file__
+    def index(self, *arg):
+        return 'the arg is: {0}'.format(arg)
