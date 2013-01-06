10437172bf07741b33ec2ff3b61df383f54f537a
Andrea Belvedere <andrea.belvedere@gengo.com>
2013-01-06 21:13:38 +0900

    update lib controller adding useful is_ methods

--- napoli/lib/controller.py
+++ napoli/lib/controller.py
@@ -7,4 +7,30 @@ from webob import Response
 class Controller(object):
     def __init__(self):
         self.request = None
-        self.response = Response()
+        self.response = None
+
+    @property
+    def is_post(self):
+        return 'POST' == self.method
+
+    @property
+    def is_get(self):
+        return 'GET' == self.method
+
+    @property
+    def is_head(self):
+        return 'HEAD' == self.method
+
+    @property
+    def is_ajax(self):
+        """
+        only works if X-Requested-With header is present and equal to XMLHttpRequest
+        """
+        return self.request.is_xhr
+
+    @property
+    def method(self):
+        try:
+            return self.request.method
+        except:
+            return None
